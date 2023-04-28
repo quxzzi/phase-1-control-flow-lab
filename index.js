7038be060ffadf10425415d20866bf4c67fdaf52
Kevin Dean <kdean1011@gmail.com>
2023-04-28 10:42:02 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+  return `This one is on me!`
+  }
+  if (someValue >= 400 && someValue <= 2000){
+  return 'That will be twenty bucks.'
+  }
+  if (someValue >= 2000 && someValue <= 2499){
+    return `I will gladly take your thirty bucks.`
+  }
+  if (someValue >= 2500){
+    return `No can do.`
+  } 
+}
+function ternaryCheckCity(City){
+  return City === `NYC`?`Ok, sounds good.`:`No go.`
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case `generous`:
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
+  }
 }
